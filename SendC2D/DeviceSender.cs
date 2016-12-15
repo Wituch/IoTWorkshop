6bd7d91f083afd4798c36422f46f7530041cb572
Wojciech <wituch95@poczta.onet.pl>
2016-12-15 11:44:01 +0100

    Add SendC2D

---
+++ SendC2D/DeviceSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace SendC2D
+{
+    public class DeviceSender
+    {
+        private ServiceClient _serviceClient;
+        private string _connectionString = "HostName=SimpleWeatherHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=5fhYg1aodzXETRDX3cr6lVU6hoASsOLIKv+VErQ4AVo=";
+
+        public void SendMessage()
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            _serviceClient = ServiceClient.CreateFromConnectionString(_connectionString);
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            SendCloudToDeviceMessageAsync("Cloud to device message").Wait();
+            Console.ReadLine();
+        }
+
+        private async Task SendCloudToDeviceMessageAsync(string message)
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(message));
+            await _serviceClient.SendAsync("myRaspi", commandMessage);
+        }
+    }
+}
